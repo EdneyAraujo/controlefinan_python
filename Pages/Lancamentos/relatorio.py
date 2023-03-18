c7c6dca1749d276c7eb2c4cabd143963615f9fd0
Edney Araujo <edney_vk@hotmail.com>
2023-03-18 18:25:57 -0400

    Conclusão Delete
    Conclusão da função delete e inicio estruturação dos graficos

---
+++ Pages/Lancamentos/relatorio.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+
+def Relatorio():
+    st.title("Relatorio") 
--- services/database.py
+++ services/database.py
@@ -1,8 +1,8 @@
 import pyodbc
 
-server = '0224\SQLEXPRESS' 
+server = 'HfdfdSS' 
 database = 'controle_financeiro_python' 
 username = 'sa' 
-password = '12233'
+password = '30dfdfd00'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=no;UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
